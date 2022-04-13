797a1fff73bf8cf176870ad1a7057e468d77a4df
Vasyl Kozak <sasdaiv@gmail.com>
2022-04-13 11:28:07 +0300

    Додавання кнопки "виконано" на головній сторінці

--- reminder/main/urls.py
+++ reminder/main/urls.py
@@ -4,6 +4,7 @@ from . import  views
 
 urlpatterns = [
     path('', views.index, name = 'home'),
+    path('<int:pk>',views.TaskDeleteView.as_view(), name = 'delete'),
     path('about', views.about, name = 'about'),
     path('create', views.create, name = 'create')
 ]
--- reminder/main/views.py
+++ reminder/main/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render,redirect
 from .models import Task
 from .forms import TaskForm
-
+from django.views.generic import DeleteView
 
 def index(request):
     tasks = Task.objects.order_by('-id')
     return render(request, 'main/index.html', {'title': "Головна сторінка", 'tasks': tasks})
+
+
+def TaskDeleteView(DeleteView):
+    model = Task
+    success_url = '/'
+    template_name = 'main/delete.html'
+    context_object_name = 'task'
 
 
 def about(request):
