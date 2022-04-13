8eadd1b7b10cf14fc011632a95a51de722266665
Vasyl Kozak <sasdaiv@gmail.com>
2022-04-13 10:27:15 +0300

    Initial commit

---
+++ reminder/main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField('Назва',max_length=50)
+    task = models.TextField('Завдання')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Завдання"
+        verbose_name_plural = "Завдання"
